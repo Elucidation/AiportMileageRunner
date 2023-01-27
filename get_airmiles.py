f312b5e73a1147f4224c2f849a83f99ad185b41e
Sam <elucidation@gmail.com>
2023-01-26 17:06:48 -0800

    Brute force mileage run

--- get_airmiles.py
+++ get_airmiles.py
@@ -1,53 +1,72 @@
-import requests
-import bs4
 import pandas as pd
+import matplotlib.pyplot as plt
+from collections import deque
 
-# ex. https://www.airmilescalculator.com/distance/sea-to-hnl/ for SEA -> HNL
-def getMiles(a: str, b: str) -> int:
-    r = requests.get(f'https://www.airmilescalculator.com/distance/{a}-to-{b}/')
-    if (r.status_code != 200):
-        raise Exception('Bad status: ', r)
-
-    soup = bs4.BeautifulSoup(r.content, 'html.parser')
-    divs = soup.findAll("div", {"class": "numberline"})
-    if len(divs) <= 0:
-        raise Exception('HTML has changed', divs)
-    
-    return int(divs[0].text)
-
-# a = 'sea'
-# b = 'bos'
-# miles = getMiles(a,b)
-# print(f'It is {miles} mi from {a} to {b}')
-
-# with open('air_prices.txt', 'r') as f:
-#     rows = f.readlines()
-#     data = []
-#     for row in rows:
-#         parts = row.strip().split()
-#         if len(parts) == 3:
-#             # parts.append(0) # Add zero miles
-#             parts.append(getMiles(parts[0], parts[1]))
-#         if len(parts) == 4:
-#             parts[3] = int(parts[3])
-#         else:
-#             continue # Bad data, skip 
-#         # parts = [A B Price Dist]
-#         parts[2] = int(parts[2].lstrip('$'))
-#         data.append(parts)
-# for (a,b,price,dist) in data:
-#     print(f'{a} -> {b} = {dist} miles, ${price}')
-
-def loadAirportCodeToPositions():
-    pos_data = pd.read_csv(r'iata-icao.csv', sep=',')
-    pos_data = pos_data[pos_data['country_code'] == 'US'] # Strip to just US
-    p = pos_data[['iata', 'latitude', 'longitude']].set_index('iata')
-    p = p.astype({'latitude':'float64', 'longitude': 'float64'})
-    return p.T.to_dict('list')
-
-airport_code_to_position = loadAirportCodeToPositions()
-
-data = pd.read_csv(r'air_prices.csv')
-print(data)
-
+airport_locations = pd.read_csv(r'airport_locations.csv').set_index('iata')
+airport_code_to_position = airport_locations.T.to_dict('list')
 # ex: airport_code_to_position['BOS'] --> [42.36429977, -71.00520325]
+
+trip_miles = pd.read_csv('trip_miles_costs.csv')
+# print(trip_miles[['start','destination','miles']])
+
+# plt.xlim(-160,-60)
+# plt.ylim(20,70)
+
+# places_in_trips = set()
+# for start, destination, price, miles in trip_miles.to_numpy():
+#     places_in_trips.add(start)
+#     places_in_trips.add(destination)
+
+#     sy, sx = airport_code_to_position[start]
+#     dy, dx = airport_code_to_position[destination]
+
+#     mx, my = (sx+dx)/2, (sy+dy)/2
+#     plt.plot([sx, dx], [sy, dy], '-')
+#     plt.text(mx, my, f'{miles}')
+
+
+# for airport in airport_code_to_position:
+#     if airport not in places_in_trips:
+#         continue
+#     lat, long = airport_code_to_position[airport]
+#     plt.plot(long, lat, 'o')
+#     plt.text(long, lat, airport)
+
+# plt.grid()
+# plt.show()
+
+def get_edges(iata):
+    return trip_miles[trip_miles['start'] == iata][['destination','miles']].to_numpy().tolist()
+    # [[dest, miles],...]
+
+
+def get_mileage_run(goal_miles=10000, start='SEA', end='SEA', max_flights=6):
+    start_block = [0, [start]] # [miles, path]
+    stack = deque([start_block])
+    runs = []
+    while stack:
+        miles, path = stack.popleft()
+        if len(path) >= max_flights:
+            # runs.append([miles, path])
+            continue
+        if miles > goal_miles and path[-1] == end:
+            runs.append([miles, path])
+            continue
+        
+        for neighbor, dist in get_edges(path[-1]):
+            new_path = path + [neighbor]
+            new_miles = miles + dist
+            stack.append([new_miles, new_path])
+            # todo still
+        # print(stack)
+    runs = sorted(runs, reverse=True)
+    return runs
+
+runs = get_mileage_run()
+for miles, route in runs:
+    print(f'Route: {route} - {miles} mi')
+
+# for start, destination, price, miles in trip_miles.to_numpy():
+#     lat_s, lon_s = airport_code_to_position[start]
+#     lat_d, lon_d = airport_code_to_position[destination]
+#     print(f'{start} {destination} {[[lon_s, lat_s], [lon_d, lat_d]]}')
